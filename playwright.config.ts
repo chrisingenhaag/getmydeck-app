165235fcb7a4607b975b088a318758d940c20241
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-06-26 20:50:00 +0200

    Configure json-server for local dev and test

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,7 +4,9 @@ const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'npm run build && npm run preview',
 		port: 3000
-	}
+	},
+	testDir: './tests',
+	testMatch: /.*\.e2e\.ts/,
 };
 
 export default config;
