e3725bcc8582802a03ba738e339cc6df3911e7e3
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-06-28 22:20:52 +0200

    Some updates

--- src/lib/Description.svelte
+++ src/lib/Description.svelte
@@ -1,20 +1,23 @@
 <article class="">
   <h1 class="text-center">How long to get my Steam Deck?</h1>
-  <p>Inspired from the reddit Steam Deck Order Email Megathreads <a target="_blank" href="https://www.reddit.com/r/SteamDeck/search?q=flair_name%3A%22MEGATHREAD%22&restrict_sr=1">(list of them here)</a> 
-    I wanted to give an alternative way of getting actual information
-    besides talking to the deckbot within the reddit. 
+  <p>
+    Inspired from the reddit Steam Deck Order Email Megathreads <a
+      target="_blank"
+      href="https://www.reddit.com/r/SteamDeck/search?q=flair_name%3A%22MEGATHREAD%22&restrict_sr=1"
+      >(list of them here)</a
+    >
+    I wanted to give an alternative way of getting actual information besides talking to the deckbot
+    within the reddit.
   </p>
   <p>
-    Anyway thanks to the great work of all guys behind the thread and their work on organizing this 
-    and making the data available. 
-    This site also relies on the data the people share in the reddit to 
-    collect information which people are able to
-    order their steam deck based on the reservation time, their region and their version.
+    Anyway thanks to the great work of all guys behind the thread and their work on organizing this
+    and making the data available. This site also relies on the data the people share in the reddit
+    to collect information which people are able to order their steam deck based on the reservation
+    time, their region and their version.
   </p>
   <p>
-    Here you just need to enter your data to get your information about how far 
-    away you might be from ordering your steam deck. Every week 
-    you can just come back and see if something has changed (without a reddit 
-    account and talking to deckbot every week).
+    Here you just need to enter your data to get your information about how far away you might be
+    from ordering your steam deck. Every week you can just come back and see if something has
+    changed (without a reddit account and talking to deckbot every week).
   </p>
 </article>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -6,97 +6,98 @@
   import Changelog from '$lib/Changelog.svelte';
   import Footer from '$lib/Footer.svelte';
 
-  const REMEMBERME_KEY = "urn:getmydeck:rememberme";
+  const REMEMBERME_KEY = 'urn:getmydeck:rememberme';
 
   let reservationTime: number;
-  let selectedRegion: Region
-  let selectedVersion: Version
+  let selectedRegion: Region;
+  let selectedVersion: Version;
 
-  let reserationTimeValid: boolean = true
-  let selectedRegionValid: boolean = true
-  let selectedVersionValid: boolean = true
+  let reserationTimeValid = true;
+  let selectedRegionValid = true;
+  let selectedVersionValid = true;
 
-  let reservationTimeHumanEnabled: boolean = false
+  let reservationTimeHumanEnabled = false;
   let reserationTimeHuman: string;
 
   let rememberme = false;
 
   let regions = [
     { id: 0, text: `empty`, value: undefined },
-		{ id: 1, text: `US`, value: `US` },
-		{ id: 2, text: `UK`, value: `UK` },
-		{ id: 3, text: `EU`, value: `EU` }
-	];
+    { id: 1, text: `US`, value: `US` },
+    { id: 2, text: `UK`, value: `UK` },
+    { id: 3, text: `EU`, value: `EU` }
+  ];
 
   let versions = [
     { id: 0, text: `empty`, value: undefined },
-		{ id: 1, text: `64GB`, value: `64` },
-		{ id: 2, text: `256GB`, value: `256` },
-		{ id: 3, text: `512GB`, value: `512` }
-	];
+    { id: 1, text: `64GB`, value: `64` },
+    { id: 2, text: `256GB`, value: `256` },
+    { id: 3, text: `512GB`, value: `512` }
+  ];
 
   function resetValidation() {
-    selectedRegionValid = true
-    selectedVersionValid = true
-    reserationTimeValid = true
+    selectedRegionValid = true;
+    selectedVersionValid = true;
+    reserationTimeValid = true;
   }
 
   function validateForm() {
-    resetValidation()
+    resetValidation();
 
-    if(selectedRegion === undefined) {
-      selectedRegionValid = false
+    if (selectedRegion === undefined) {
+      selectedRegionValid = false;
     }
-    if(selectedVersion === undefined) {
-      selectedVersionValid = false
+    if (selectedVersion === undefined) {
+      selectedVersionValid = false;
     }
-    if(reservationTime === undefined || 
-        reservationTime === null || 
-        reservationTime.toString().length !== 10) {
-      reserationTimeValid = false
+    if (
+      reservationTime === undefined ||
+      reservationTime === null ||
+      reservationTime.toString().length !== 10
+    ) {
+      reserationTimeValid = false;
     }
-    return selectedRegionValid && selectedVersionValid && reserationTimeValid
+    return selectedRegionValid && selectedVersionValid && reserationTimeValid;
   }
 
   function handleSubmit() {
-    if(validateForm()) {
-      
-      const resultLink = `s/${selectedRegion}/${selectedVersion}/${reservationTime}`
+    if (validateForm()) {
+      const resultLink = `s/${selectedRegion}/${selectedVersion}/${reservationTime}`;
 
-      if(rememberme === true) {
-        saveRememberme()
+      if (rememberme === true) {
+        saveRememberme();
       } else {
         localStorage.clear();
       }
 
-      window.location.assign(resultLink)
+      window.location.assign(resultLink);
     }
-	}
+  }
 
   let saveRememberme = () => {
     let valueToStore = {
       region: selectedRegion,
       version: selectedVersion,
       timestamp: reservationTime
-    }
+    };
     localStorage.setItem(REMEMBERME_KEY, JSON.stringify(valueToStore));
-  }
+  };
 
   $: {
-    if(reservationTimeHumanEnabled) {
-      let humanTime = Date.parse(reserationTimeHuman) / 1000
-      reservationTime = humanTime
+    if (reservationTimeHumanEnabled) {
+      let humanTime = Date.parse(reserationTimeHuman) / 1000;
+      reservationTime = humanTime;
     }
   }
 
   onMount(async () => {
     let storedString = localStorage.getItem(REMEMBERME_KEY);
     if (storedString !== null) {
-      let storedValues = JSON.parse(storedString)
-      reservationTime = storedValues.timestamp
-      selectedRegion = storedValues.region
-      selectedVersion = storedValues.version
-      rememberme = true
+      let storedValues = JSON.parse(storedString);
+      reservationTime = storedValues.timestamp;
+      selectedRegion = storedValues.region;
+      selectedVersion = storedValues.version;
+      rememberme = true;
     }
   });
 </script>
@@ -107,80 +108,137 @@
 
 <div class="container mx-auto shadow-md p-5 mt-3 md:w-1/2 bg-white prose">
   <div class="grid grid-cols-1 gap-6 content-center">
-    
     <Description />
 
     <form on:submit|preventDefault={handleSubmit}>
       <div class="">
-        <label for="region" class="text-gray-700">In which region did you preorder your Steam Deck?</label>
-        <select class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedRegionValid ? 'bg-red-50 border border-red-500 text-red-900' : ''}" id="region" name="region" bind:value={selectedRegion} on:change={() => selectedRegionValid = true}>
+        <label for="region" class="text-gray-700"
+          >In which region did you preorder your Steam Deck?</label
+        >
+        <select
+          class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedRegionValid
+            ? 'bg-red-50 border border-red-500 text-red-900'
+            : ''}"
+          id="region"
+          name="region"
+          bind:value={selectedRegion}
+          on:change={() => (selectedRegionValid = true)}
+        >
           {#each regions as region}
-          <option label={region.text} value={region.value}>
-            {region.text}
-          </option>
+            <option label={region.text} value={region.value}>
+              {region.text}
+            </option>
           {/each}
         </select>
-        {#if !selectedRegionValid }
-        <span class="text-sm text-red-600 dark:text-red-500">Please select a region</span>
+        {#if !selectedRegionValid}
+          <span class="text-sm text-red-600 dark:text-red-500">Please select a region</span>
         {/if}
       </div>
       <div class="">
         <label for="version" class="text-gray-700">Which version did you reserve?</label>
-        <select class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedVersionValid ? 'bg-red-50 border border-red-500 text-red-900' : ''}" id="version" name="version" bind:value={selectedVersion} on:change={() => selectedVersionValid = true}>
+        <select
+          class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedVersionValid
+            ? 'bg-red-50 border border-red-500 text-red-900'
+            : ''}"
+          id="version"
+          name="version"
+          bind:value={selectedVersion}
+          on:change={() => (selectedVersionValid = true)}
+        >
           {#each versions as version}
-          <option label={version.text} value={version.value}>
-            {version.text}
-          </option>
+            <option label={version.text} value={version.value}>
+              {version.text}
+            </option>
           {/each}
         </select>
-        {#if !selectedVersionValid }
-        <span class="text-sm text-red-600 dark:text-red-500">Please select a version</span>
+        {#if !selectedVersionValid}
+          <span class="text-sm text-red-600 dark:text-red-500">Please select a version</span>
         {/if}
       </div>
-    
-      {#if reservationTimeHumanEnabled }
-      <div class="">
-        <label for="reserationTimeHuman" class="text-gray-700">Your reservation time (in seconds from 01.01.1970 example: 1627022437). Get it like 
-          described in the <a target="_blank" href="https://www.reddit.com/r/SteamDeck/comments/ui642q/introducing_deckbot/">reddit DeckBot description</a>.
-        </label>
-        <input type="datetime-local" class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid ? 'bg-red-50 border border-red-500 text-red-900' : ''}" name="reserationTimeHuman" id="reserationTimeHuman" bind:value={reserationTimeHuman}/>
-        {#if !reserationTimeValid }
-        <span class="text-sm text-red-600 dark:text-red-500">Please enter a valid datetime</span>
-        {/if}
-      </div>
+
+      {#if reservationTimeHumanEnabled}
+        <div class="">
+          <label for="reserationTimeHuman" class="text-gray-700"
+            >Your reservation date and time.
+          </label>
+          <input
+            type="datetime-local"
+            class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid
+              ? 'bg-red-50 border border-red-500 text-red-900'
+              : ''}"
+            name="reserationTimeHuman"
+            id="reserationTimeHuman"
+            bind:value={reserationTimeHuman}
+          />
+          {#if !reserationTimeValid}
+            <span class="text-sm text-red-600 dark:text-red-500">Please enter a valid datetime</span
+            >
+          {/if}
+        </div>
       {:else}
-      <div class="">
-        <label for="reserationTime" class="text-gray-700">Your reservation time (in seconds from 01.01.1970 example: 1627022437). Get it like 
-          described in the <a target="_blank" href="https://www.reddit.com/r/SteamDeck/comments/ui642q/introducing_deckbot/">reddit DeckBot description</a> 
-          or switch to a datetime picker with the sitch below.
-        </label>
-        <input type="number" class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid ? 'bg-red-50 border border-red-500 text-red-900' : ''}" name="reservationTime" id="reserationTime" bind:value={reservationTime}/>
-        {#if !reserationTimeValid }
-        <span class="text-sm text-red-600 dark:text-red-500">Please enter a valid timestamp (10 digits)</span>
-        {/if}
-      </div>
+        <div class="">
+          <label for="reserationTime" class="text-gray-700"
+            >Your reservation time (in seconds from 01.01.1970 example: 1627022437). Get it like
+            described in the <a
+              target="_blank"
+              href="https://www.reddit.com/r/SteamDeck/comments/ui642q/introducing_deckbot/"
+              >reddit DeckBot description</a
+            >
+            or switch to a datetime picker with the sitch below.
+          </label>
+          <input
+            type="number"
+            class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid
+              ? 'bg-red-50 border border-red-500 text-red-900'
+              : ''}"
+            name="reservationTime"
+            id="reserationTime"
+            bind:value={reservationTime}
+          />
+          {#if !reserationTimeValid}
+            <span class="text-sm text-red-600 dark:text-red-500"
+              >Please enter a valid timestamp (10 digits)</span
+            >
+          {/if}
+        </div>
       {/if}
 
       <div class="mt-2">
         <label for="reservationTimeHumanEnabled" class="flex items-center cursor-pointer relative">
-          <input bind:checked={reservationTimeHumanEnabled} type="checkbox" id="reservationTimeHumanEnabled" class="sr-only">
-          <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full"></div>
-          <span class="ml-3 text-gray-900 text-sm font-medium">Select human readable date and time</span>
+          <input
+            bind:checked={reservationTimeHumanEnabled}
+            type="checkbox"
+            id="reservationTimeHumanEnabled"
+            class="sr-only"
+          />
+          <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full" />
+          <span class="ml-3 text-gray-900 text-sm font-medium"
+            >Select human readable date and time</span
+          >
         </label>
       </div>
 
       <label class="block mt-3">
-        <input type="checkbox" class="form-input rounded-md shadow-sm" name="rememberme" id="rememberme" bind:checked={rememberme}/>
+        <input
+          type="checkbox"
+          class="form-input rounded-md shadow-sm"
+          name="rememberme"
+          id="rememberme"
+          bind:checked={rememberme}
+        />
         <span class="text-gray-700">Remember me</span>
       </label>
 
       <label class="">
-        <button class="px-4 py-2 mt-5 font-semibold text-sm bg-sky-900 active:bg-sky-600 text-white rounded-lg shadow-sm" type="submit">
+        <button
+          class="px-4 py-2 mt-5 font-semibold text-sm bg-sky-900 active:bg-sky-600 text-white rounded-lg shadow-sm"
+          type="submit"
+        >
           Get my current preorder status
         </button>
       </label>
     </form>
-    
+
     <Changelog />
 
     <Footer />
