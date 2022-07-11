24f15d1cae5eb552e221ad8b5ec4687938dacd82
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-07-11 13:33:56 +0200

    Fix css layout on index page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -108,12 +108,13 @@
 
 <Description />
 
-<form on:submit|preventDefault={handleSubmit}>
-  <div class="">
-    <label for="region" class="text-gray-700"
+<div class="mb-3">
+  <form on:submit|preventDefault={handleSubmit}>
+    <div class="">
+      <label for="region" class="text-gray-700"
       >In which region did you preorder your Steam Deck?</label
-    >
-    <select
+      >
+      <select
       class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedRegionValid
         ? 'bg-red-50 border border-red-500 text-red-900'
         : ''}"
@@ -121,70 +122,70 @@
       name="region"
       bind:value={selectedRegion}
       on:change={() => (selectedRegionValid = true)}
-    >
+      >
       {#each regions as region}
-        <option label={region.text} value={region.value}>
-          {region.text}
-        </option>
+      <option label={region.text} value={region.value}>
+        {region.text}
+      </option>
       {/each}
     </select>
     {#if !selectedRegionValid}
-      <span class="text-sm text-red-600 dark:text-red-500">Please select a region</span>
+    <span class="text-sm text-red-600 dark:text-red-500">Please select a region</span>
     {/if}
   </div>
   <div class="">
     <label for="version" class="text-gray-700">Which version did you reserve?</label>
     <select
-      class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedVersionValid
+    class="form-select block rounded-md shadow-sm w-full mt-1 {!selectedVersionValid
         ? 'bg-red-50 border border-red-500 text-red-900'
         : ''}"
       id="version"
       name="version"
       bind:value={selectedVersion}
       on:change={() => (selectedVersionValid = true)}
-    >
+      >
       {#each versions as version}
-        <option label={version.text} value={version.value}>
-          {version.text}
-        </option>
+      <option label={version.text} value={version.value}>
+        {version.text}
+      </option>
       {/each}
     </select>
     {#if !selectedVersionValid}
-      <span class="text-sm text-red-600 dark:text-red-500">Please select a version</span>
+    <span class="text-sm text-red-600 dark:text-red-500">Please select a version</span>
     {/if}
   </div>
-
+  
   {#if reservationTimeHumanEnabled}
-    <div class="">
-      <label for="reserationTimeHuman" class="text-gray-700"
-        >When did you place your reservation? This is is interpreted as your local timezone and will be converted to UTC automatically.
-      </label>
-      <input
-        type="datetime-local"
-        class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid
+  <div class="">
+    <label for="reserationTimeHuman" class="text-gray-700"
+    >When did you place your reservation? This is is interpreted as your local timezone and will be converted to UTC automatically.
+  </label>
+  <input
+  type="datetime-local"
+  class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid
           ? 'bg-red-50 border border-red-500 text-red-900'
           : ''}"
         name="reserationTimeHuman"
         id="reserationTimeHuman"
         bind:value={reserationTimeHuman}
-      />
-      {#if !reserationTimeValid}
+        />
+        {#if !reserationTimeValid}
         <span class="text-sm text-red-600 dark:text-red-500">Please enter a valid datetime</span
-        >
-      {/if}
-    </div>
-  {:else}
-    <div class="">
-      <label for="reserationTime" class="text-gray-700"
-        >What is your reservation time (in seconds from 01.01.1970 example: 1627022437)? Get it like
-        described in the <a
+          >
+          {/if}
+        </div>
+        {:else}
+        <div class="">
+          <label for="reserationTime" class="text-gray-700"
+          >What is your reservation time (in seconds from 01.01.1970 example: 1627022437)? Get it like
+          described in the <a
           target="_blank"
           href="https://www.reddit.com/r/SteamDeck/comments/ui642q/introducing_deckbot/"
           >reddit DeckBot description</a
-        >
-        or switch to a datetime picker with the sitch below.
-      </label>
-      <input
+          >
+          or switch to a datetime picker with the sitch below.
+        </label>
+        <input
         type="number"
         class="form-input block rounded-md shadow-sm w-full mt-1 {!reserationTimeValid
           ? 'bg-red-50 border border-red-500 text-red-900'
@@ -192,63 +193,64 @@
         name="reservationTime"
         id="reserationTime"
         bind:value={reservationTime}
-      />
-      {#if !reserationTimeValid}
+        />
+        {#if !reserationTimeValid}
         <span class="text-sm text-red-600 dark:text-red-500"
-          >Please enter a valid timestamp (10 digits)</span
+        >Please enter a valid timestamp (10 digits)</span
         >
+        {/if}
+      </div>
       {/if}
-    </div>
-  {/if}
-
-  <div class="mt-2">
-    <label for="reservationTimeHumanEnabled" class="flex items-center cursor-pointer relative">
-      <input
-        bind:checked={reservationTimeHumanEnabled}
+      
+      <div class="mt-2">
+        <label for="reservationTimeHumanEnabled" class="flex items-center cursor-pointer relative">
+          <input
+          bind:checked={reservationTimeHumanEnabled}
+          type="checkbox"
+          id="reservationTimeHumanEnabled"
+          class="sr-only"
+          />
+          <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full" />
+          <span class="ml-3 text-gray-900 text-sm font-medium"
+          >Select human readable date and time</span
+          >
+        </label>
+      </div>
+      
+      <label class="block mt-3">
+        <input
         type="checkbox"
-        id="reservationTimeHumanEnabled"
-        class="sr-only"
-      />
-      <div class="toggle-bg bg-gray-200 border-2 border-gray-200 h-6 w-11 rounded-full" />
-      <span class="ml-3 text-gray-900 text-sm font-medium"
-        >Select human readable date and time</span
-      >
+        class="form-input rounded-md shadow-sm"
+        name="rememberme"
+        id="rememberme"
+        bind:checked={rememberme}
+        />
+        <span class="text-gray-700">Remember me</span>
+      </label>
+      
+      <label class="">
+        <button
+        class="px-4 py-2 mt-5 font-semibold text-sm bg-sky-900 active:bg-sky-600 text-white rounded-lg shadow-sm"
+        type="submit"
+        >
+        Get my current preorder status
+      </button>
     </label>
-  </div>
-
-  <label class="block mt-3">
-    <input
-      type="checkbox"
-      class="form-input rounded-md shadow-sm"
-      name="rememberme"
-      id="rememberme"
-      bind:checked={rememberme}
-    />
-    <span class="text-gray-700">Remember me</span>
-  </label>
-
-  <label class="">
-    <button
-      class="px-4 py-2 mt-5 font-semibold text-sm bg-sky-900 active:bg-sky-600 text-white rounded-lg shadow-sm"
-      type="submit"
-    >
-      Get my current preorder status
-    </button>
-  </label>
-</form>
-
+  </form>
+</div>
+  
 <Changelog />
-
+  
 <style>
-  .toggle-bg:after {
-    content: '';
-    @apply absolute top-0.5 left-0.5 bg-white border border-gray-300 rounded-full h-5 w-5 transition shadow-sm;
-  }
-  input:checked + .toggle-bg:after {
-    transform: translateX(100%);
-    @apply border-white;
-  }
-  input:checked + .toggle-bg {
-    @apply bg-blue-600 border-blue-600;
-  }
+.toggle-bg:after {
+  content: '';
+  @apply absolute top-0.5 left-0.5 bg-white border border-gray-300 rounded-full h-5 w-5 transition shadow-sm;
+}
+input:checked + .toggle-bg:after {
+  transform: translateX(100%);
+  @apply border-white;
+}
+input:checked + .toggle-bg {
+  @apply bg-blue-600 border-blue-600;
+}
 </style>
