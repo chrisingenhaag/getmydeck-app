0865c79877bfc4ade1719978442188f7f9444667
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-08-20 11:03:26 +0200

    Fix error page layout

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,23 +2,16 @@
 <script>
   import { dev } from '$app/env';
   import { page } from '$app/stores';
-  import Title from '$lib/Title.svelte';
 </script>
 
 <svelte:head>
   <title>{$page.status}</title>
 </svelte:head>
 
-<div class="container mx-auto shadow-md p-5 mt-3 md:w-1/2 bg-white prose">
-  <div class="grid grid-cols-1 gap-6 content-center">
-    <article class="">
-      <Title/>
-      <p>Oops, this page does not exist</p>
-      <a class="link" href="/">Back to Home</a>
 
-      {#if dev && $page.error?.stack}
-       <pre>{$page.error.stack}</pre>
-      {/if}
-    </article>
-  </div>
-</div>
+<p>Oops, this page does not exist</p>
+<a class="link" href="/">Back to Home</a>
+
+{#if dev && $page.error?.stack}
+  <pre>{$page.error.stack}</pre>
+{/if}
