714c34c7df955f2abf0de5f95b3a135d98ae0a76
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-08-28 21:41:14 +0200

    Extract changelog to own page

---
+++ src/routes/changelog/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+import Changelog from "$lib/Changelog.svelte";
+
+  import GetMyDeckStatistics from "$lib/GetMyDeckStatistics.svelte";
+</script>
+
+<svelte:head>
+  <title>GetMyDeck - Changelog</title>
+</svelte:head>
+
+<div class="text-sm breadcrumbs not-prose">
+  <ul>
+    <li><a class="link" href="/">Start</a></li> 
+    <li>Changelog</li>
+  </ul>
+</div>
+
+<div class="prose">
+  <h3>Changelog</h3>
+</div>
+
+<Changelog />
+
