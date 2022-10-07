2703f318f402e9c20e8bc633658b3aaf2196014b
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-10-07 21:37:50 +0200

    Replace startpage form with deprecation note

---
+++ src/lib/Deprecation.svelte
@@ -0,0 +1,29 @@
+<div class="prose mb-3">
+  <p>
+    As of october 6, 2022 valve announced officially that steam deck orders are possible without reservation.
+    This means that GetMyDeck will no longer be required.
+    The <a href="/statistics">results page</a> will be still available to review the progress we as a reddit community collected.
+  </p>
+  <p>
+    It was a pleasure to help the community to make u/Fammy`s data accessible with this site and I hope everybody will now or very soon have 
+    a lot of fun with this great device.<br>
+
+    Cheers, u/Labidou51
+  </p>
+  <p>
+    P.S.: For the Komodo backed distribution in asia it is not sure yet if crowd sourced data collection like it was for GetMyDeck will be possible. 
+    We will have to wait until shipment in this region starts to see if GetMyDeck can back up the waiting time.
+  </p>
+</div>
+
+<div class="grid place-content-center mb-3">
+  <blockquote class="twitter-tweet">
+    <p lang="en" dir="ltr">BIG NEWS today!<br>
+      ✨ Steam Deck is now available without a reservation! Order one, we&#39;ll ship it to you: <a href="https://t.co/uVdBCpJVuX">https://t.co/uVdBCpJVuX</a><br>
+      📺 The Docking Station is available, too! Connect to peripherals and displays in style: <a href="https://t.co/QqLoKsvASs">https://t.co/QqLoKsvASs</a><br>
+      💾 Plus, lots of new software features: <a href="https://t.co/64VzQeisuZ">pic.twitter.com/64VzQeisuZ</a>
+    </p>
+    &mdash; Steam Deck (@OnDeck) <a href="https://twitter.com/OnDeck/status/1578088497759793152?ref_src=twsrc%5Etfw">October 6, 2022</a>
+  </blockquote> 
+</div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,8 +2,8 @@
   import { onMount } from 'svelte';
 
   import type { Region, Version } from '$lib/DeckTypes';
-  import Description from '$lib/Description.svelte';
   import { DATA_KEY } from '$lib/Constants';
+  import Deprecation from '$lib/Deprecation.svelte';
 
   const REMEMBERME_KEY = 'urn:getmydeck:rememberme';
 
@@ -124,149 +124,7 @@
 
 <svelte:head>
   <title>GetMyDeck - Calculator</title>
+  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8" ></script>
 </svelte:head>
 
-<Description />
-
-<div class="mb-3 prose">
-  <form on:submit|preventDefault={handleSubmit}>
-    <div class="">
-      <label for="region" class="text-gray-700"
-      >In which region did you preorder your Steam Deck?</label
-      >
-      <select
-      class="select select-bordered w-full max-w {!selectedRegionValid
-        ? 'select-error'
-        : ''}"
-      id="region"
-      name="region"
-      bind:value={selectedRegion}
-      on:change={() => (selectedRegionValid = true)}
-      >
-      {#each regions as region}
-      <option label={region.text} value={region.value}>
-        {region.text}
-      </option>
-      {/each}
-    </select>
-    {#if !selectedRegionValid}
-    <span class="text-sm text-red-600 dark:text-red-500">Please select a region</span>
-    {/if}
-  </div>
-  <div class="form-control w-full max-w">
-    <label for="version" class="text-gray-700">Which version did you reserve?</label>
-    <select
-    class="select select-bordered w-full max-w {!selectedVersionValid
-        ? 'select-error'
-        : ''}"
-      id="version"
-      name="version"
-      bind:value={selectedVersion}
-      on:change={() => (selectedVersionValid = true)}
-      >
-      {#each versions as version}
-      <option label={version.text} value={version.value}>
-        {version.text}
-      </option>
-      {/each}
-    </select>
-    {#if !selectedVersionValid}
-    <span class="text-sm text-red-600 dark:text-red-500">Please select a version</span>
-    {/if}
-  </div>
-  
-  {#if reservationTimeHumanEnabled}
-  <div class="form-control w-full max-w">
-    <label for="reserationTimeHuman" class="text-gray-700"
-    >When did you place your reservation? This is is interpreted as your local timezone and will be converted to UTC automatically.
-  </label>
-  <input
-  type="datetime-local"
-  class="input input-bordered input-md w-full max-w  {!reserationTimeValid
-          ? 'input-error'
-          : ''}"
-        name="reserationTimeHuman"
-        id="reserationTimeHuman"
-        bind:value={reserationTimeHuman}
-        />
-        {#if !reserationTimeValid}
-        <span class="text-sm text-red-600 dark:text-red-500">Please enter a valid datetime</span
-          >
-          {/if}
-        </div>
-        {:else}
-        <div class="form-control w-full max-w">
-          <label for="reserationTime" class="text-gray-700">
-            You need your reservation time (in seconds from 01.01.1970 example: 1627022437).
-            <label for="timestamp-modal" class="link modal-button">How to get your rtReserveTime?</label>
-            Or switch to a datetime picker with the toggle below.
-          </label>
-          
-        <input
-        type="number"
-        class="input input-bordered input-md w-full max-w {!reserationTimeValid
-          ? 'input-error'
-          : ''}"
-        name="reservationTime"
-        id="reserationTime"
-        bind:value={reservationTime}
-        />
-        {#if !reserationTimeValid}
-        <span class="text-sm text-red-600 dark:text-red-500"
-        >Please enter a valid timestamp (10 digits)</span
-        >
-        {/if}
-      </div>
-      {/if}
-      
-      <div class="form-control mt-2 flex flex-row">
-        <label class="label cursor-pointer" for="reservationTimeHumanEnabled">
-          <input bind:checked={reservationTimeHumanEnabled} id="reservationTimeHumanEnabled" type="checkbox" class="toggle toggle-secondary" />
-          <span class="label-text pl-3">Select human readable date and time</span>
-        </label>
-      </div>
-      
-      <div class="form-control mt-1 flex flex-row">
-        <label class="label cursor-pointer">
-          <input bind:checked={rememberme} type="checkbox" name="rememberme" id="rememberme" class="checkbox checkbox-secondary" />
-          <span class="label-text pl-3">Remember me</span> 
-        </label>
-      </div>
-      
-      <div class="form-control mt-1 flex flex-col">
-        <button class="btn btn-primary btn-sm text-white normal-case" type="submit">Get my current preorder status</button>
-      </div>
-
-      <!-- Put this part before </body> tag -->
-      <input type="checkbox" id="timestamp-modal" class="modal-toggle"/>
-      <div id="timestamp-modal-container" class="modal modal-bottom sm:modal-middle">
-        <div class="modal-box">
-          <h3 class="font-bold text-lg">How to get your rtReserveTime?</h3>
-          <p class="py-2">
-            If you don't have your rtReserveTime, here's how to get it:
-          </p>
-          <ol>
-            <li>Log into the <a href="https://store.steampowered.com/">Steam website</a></li>
-            <li>Go to this <a href="https://store.steampowered.com/reservation/ajaxgetuserstate?rgReservationPackageIDs=%5B595603,595604,595605%5D">API link</a>. It should be a bunch of data. If you only see {'{'}"success": 21{'}'} then you aren't logged in. Repeat Step 1.</li>
-            <li>Find the text rtReserveTime and copy the number immediately thereafter. It will start with 16 and is ten digits long, like 1626460525 If the number is 0, then you've ordered yours and it's too late to find it.</li>
-          </ol>
-          <div class="modal-action">
-            <label for="timestamp-modal" class="btn btn-sm normal-case">Got it</label>
-          </div>
-        </div>
-      </div>
-
-      <!-- Put this part before </body> tag -->
-      <input type="checkbox" id="inacc-modal" class="modal-toggle" bind:checked={modalVisible} />
-      <div id="inacc-modal-container" class="modal modal-bottom sm:modal-middle">
-        <div class="modal-box">
-          <h3 class="font-bold text-lg">This may be not accurate</h3>
-          <p class="py-2">A datetime selection is not as accurate as your real timestamp from the steam api. If you want to share your data with us on reddit please use the official steam timestamp.</p>
-          <div class="modal-action">
-            <label for="inacc-modal" class="btn btn-sm normal-case" on:click={dismissModal}>Got it</label>
-          </div>
-        </div>
-      </div>
-
-  </form>
-</div>
+<Deprecation />
