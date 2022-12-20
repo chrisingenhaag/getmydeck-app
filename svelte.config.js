f4b5ae65436006f91b636cdea362356eecd95304
Christian Ingenhaag <christian.ingenhaag@googlemail.com>
2022-12-20 19:25:47 +0100

    Fix tailwindcss integration

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
@@ -5,11 +6,16 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-	preprocess: vitePreprocess(),
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      postcss: true
+    })
+  ],
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter()
+  }
 };
 
 export default config;
